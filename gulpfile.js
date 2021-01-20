68f01acea566cb49565596e55f4b4913fb96788f
IDSAD <idsad.ch@gmail.com>
2021-01-20 08:39:50 +0200

    Improved fonts

--- gulpfile.js
+++ gulpfile.js
@@ -131,9 +131,9 @@ gulp.task('otf2ttf', function () {
 
 function fontsStyle(params) {
 
-	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+	let file_content = fs.readFileSync(source_folder + '/scss/_fonts.scss');
 	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+		fs.writeFile(source_folder + '/scss/_fonts.scss', '', cb);
 		return fs.readdir(path.build.fonts, function (err, items) {
 			if (items) {
 				let c_fontname;
@@ -141,7 +141,7 @@ function fontsStyle(params) {
 					let fontname = items[i].split('.');
 					fontname = fontname[0];
 					if (c_fontname != fontname) {
-						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+						fs.appendFile(source_folder + '/scss/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
 					}
 					c_fontname = fontname;
 				}
